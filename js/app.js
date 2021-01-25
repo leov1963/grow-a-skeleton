6c355503b89098c4fe42854ac05d2430f5c0d8f3
leov1963 <leov1963@gmail.com>
2021-01-25 10:43:11 -0800

    Added start button and linked it to eventListener

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// ---set vars---
+
+// const skeleton = {
+//     age: 1,
+//     level: 1,
+//     thirst: 0,
+//     hunger: 0,
+//     darkEnergy: 10
+// }
+
+// ---make functions---
+
+// ---event listeners---
+
+// code to replace html elements when the play button is pressed
+document.querySelector("#start-button").addEventListener("click", () => {
+    document.querySelector("body").innerHTML = ""
+})
