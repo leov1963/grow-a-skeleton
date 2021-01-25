3daca7ff490ffeb328d5fa00e1ef46f37b981e3e
leov1963 <leov1963@gmail.com>
2021-01-25 12:38:37 -0800

    Updated thirstTimer func to be more efficiant/useable

--- js/app.js
+++ js/app.js
@@ -10,84 +10,17 @@ let skeleton = {
 
 // ---make functions---
 
-// code to update the stats of skeleton when game buttons are pressed
-function thirstTimer() {
-    setTimeout(myTimeout1, 3000)
-    setTimeout(myTimeout2, 6000)
-    setTimeout(myTimeout3, 9000)
-    setTimeout(myTimeout4, 12000)
-    setTimeout(myTimeout5, 15000)
-    setTimeout(myTimeout6, 18000)
-    setTimeout(myTimeout7, 21000)
-    setTimeout(myTimeout8, 24000)
-    setTimeout(myTimeout9, 27000)
-    setTimeout(myTimeout10, 30000)
-    setTimeout(myTimeout11, 31000)
-}
-
-function myTimeout1() {
-    document.querySelector("p").innerHTML = "Thirst: 1"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout2() {
-    document.querySelector("p").innerHTML = "Thirst: 2"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout3() {
-    document.querySelector("p").innerHTML = "Thirst: 3"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout4() {
-    document.querySelector("p").innerHTML = "Thirst: 4"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout5() {
-    document.querySelector("p").innerHTML = "Thirst: 5"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout6() {
-    document.querySelector("p").innerHTML = "Thirst: 6"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout7() {
-    document.querySelector("p").innerHTML = "Thirst: 7"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout8() {
-    document.querySelector("p").innerHTML = "Thirst: 8"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout9() {
-    document.querySelector("p").innerHTML = "Thirst: 9"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout10() {
-    document.querySelector("p").innerHTML = "Thirst: 10"
-    skeleton.thirst = skeleton.thirst + 1
-    console.log(skeleton.thirst)
-}
-
-function myTimeout11() {
-    document.querySelector("body").innerHTML = "<h1>Game Over!</h1>"
-}
+const thirstTimer = () => {
+    const timer = setInterval(() => {
+        skeleton.thirst++
+        console.log(skeleton.thirst)
+        document.querySelector("p").innerHTML = `Thirst: ${skeleton.thirst}`
+        if(skeleton.thirst === 10) {
+            clearInterval(timer)
+            document.querySelector("body").innerHTML = "<h1 id='game-over'>Game Over!</h1>"
+        }
+    }, 3000);
+} 
 
 // ---event listeners---
 
